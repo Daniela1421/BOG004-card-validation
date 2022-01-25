b75d1a91fbe2e2931d9b2abdd637d73a3f7d3149
Yesiblato <87024446+Yesiblato@users.noreply.github.com>
2022-01-24 22:13:22 -0500

    Cambios sobre las tarjetas de compra

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
 import validator from './validator.js';
 
-document.getElementById("validar").addEventListener("click", takeCardNumber)
+document.getElementById("validar").addEventListener("click", takeCardNumber);
 
 
 function takeCardNumber(){
     let creditCardNumber = document.getElementById("card").value;
-    validator.isValid(creditCardNumber)
+    validator.isValid(creditCardNumber);
     //validator.makkify(creditCardNumber) ?????
 
 }
 
 
+//document.getElementById("pag3").style.display = 'none';
 
+document.getElementsById("comprar1").addEventListener("click", ocultarPaginas);
 
+function ocultarPaginas(){
+    document.getElementsById('pag2').style.display = 'block';
+    document.getElementsById('pag3').style.display = 'none';
+
+}
 
 
 

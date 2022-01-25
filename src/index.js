85486e837776fc43d04b93cb42f9bcd11ace1cfb
Daniela1421 <daniela.ducuara1817@gmail.com>
2022-01-24 22:15:17 -0500

    trabajando css

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 import validator from './validator.js';
 
+/*document.getElementById("pag3").style.display = 'none';
+let btnComprar = document.getElementsByClassName("comprar")
+btnComprar.addEventListener("click", () => {
+    let sectionPagina2= document.getElementById("pag2").style = "display:none";
+    let sectionPagina3= document.getElementById("pag3").style = "display:block";
+});
+
+
+
+
+/*function ocultarPaginas(){
+    document.getElementsById('pag2').style.display = 'block';
+    document.getElementsById('pag3').style.display = 'none';
+}*/
+
+
+
+
+
+
+
+
+
+
+
 document.getElementById("validar").addEventListener("click", takeCardNumber)
 
 
 function takeCardNumber(){
     let creditCardNumber = document.getElementById("card").value;
     validator.isValid(creditCardNumber)
-    //validator.makkify(creditCardNumber) ?????
+    //validator.maskify(creditCardNumber) ?????
 
 }
 

3d2010a494bac5ee53bd9502a502fc95407ee8fd
Daniela1421 <daniela.ducuara1817@gmail.com>
2022-01-27 11:13:28 -0500

    Cambios en el header

--- src/index.js
+++ src/index.js
@@ -11,17 +11,20 @@ function takeCardNumber(){
 
 }
 
-//document.getElementById("pag3").style.display = 'none';
+//Enlace de boton comprar con la siguiente pagina
 
-document.getElementById("comprar1").addEventListener("click", ocultarPaginas);
+document.getElementById("comprar1").addEventListener("click", hidePages);
+document.getElementById("comprar2").addEventListener("click", hidePages);
+document.getElementById("comprar3").addEventListener("click", hidePages);
+document.getElementById("comprar4").addEventListener("click", hidePages);
 
-function ocultarPaginas(){
+
+function hidePages(){
     
     document.getElementById('pag2').style.display = 'none';
     document.getElementById('pag3').style.display = 'block';
     
 }
-
 
 
 /*import isValid from './validator.js';

ca71ebf37387b64a8192b940f833c8c5f0d46151
Daniela1421 <daniela.ducuara1817@gmail.com>
2022-01-31 10:01:18 -0500

    Arreglo finales

--- src/index.js
+++ src/index.js
@@ -25,18 +25,10 @@ document.getElementById("comprar2").addEventListener("click", hidePages);
 document.getElementById("comprar3").addEventListener("click", hidePages);
 document.getElementById("comprar4").addEventListener("click", hidePages);
 
-
 function hidePages(){
     
     document.getElementById('pag2').style.display = 'none';
     document.getElementById('pag3').style.display = 'block';
     
 }
-
-
-/*import isValid from './validator.js';
-console.log(isValid());
-
-import maskify from './validator.js';
-console.log(maskify());*/
 

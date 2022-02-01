0f3ce779eeec57ff2df4799fb1aebee79ed4aa28
Yesiblato <87024446+Yesiblato@users.noreply.github.com>
2022-02-01 10:54:33 -0500

    Borrado de Console.log y espacios

--- src/index.js
+++ src/index.js
@@ -1,22 +1,17 @@
 import validator from './validator.js';
 
-
 document.getElementById("validar").addEventListener("click", takeCardNumber);
 
 function takeCardNumber(){
     let creditCardNumber = document.getElementById("card").value;
     if (validator.isValid(creditCardNumber) == true){
-        document.getElementById("respuesta").innerHTML = "Su tarjeta N° " + validator.maskify(creditCardNumber) + "\n es Valida.";
-        document.getElementById("felicitaciones").innerHTML = "Pago exitoso"
-        //alert("Felicidades por su compra!")
-    } else {
-        document.getElementById("respuesta").innerHTML = "Su tarjeta N° " + validator.maskify(creditCardNumber) + "\n es Invalida.";
+        document.getElementById("respuesta").innerHTML = "Su tarjeta N° " + validator.maskify(creditCardNumber) + " es Valida.";
+        document.getElementById("felicitaciones").innerHTML = "Felicitaciones por su compra"
+        } else {
+        document.getElementById("respuesta").innerHTML = "Su tarjeta N° " + validator.maskify(creditCardNumber) + " es Invalida.";
         document.getElementById("rectificar").innerHTML = "Por favor verifique sus datos.";
-
     }
-
 }
-
 
 //Enlace de boton comprar con la siguiente pagina
 
@@ -26,8 +21,6 @@ document.getElementById("comprar3").addEventListener("click", hidePages);
 document.getElementById("comprar4").addEventListener("click", hidePages);
 
 function hidePages(){
-    
-    document.getElementById('pag2').style.display = 'none';
-    document.getElementById('pag3').style.display = 'block';
-    
+   document.getElementById('pag2').style.display = 'none';
+   document.getElementById('pag3').style.display = 'block';
 }
